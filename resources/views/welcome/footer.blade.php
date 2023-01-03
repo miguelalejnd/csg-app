9ff6a4c647c882c8a546fa561a0252fcfe8ce6bc
Miguel <m.alejnd.lpz@gmail.com>
2023-01-02 23:19:11 -0600

    Footer: Reorganización, y menor tamaño de imagenes
    Se reorganizan los elementos haciendo más visible la información de contacto,
    se cambia a diálogos más claros y directos,
    se disminuye el tamaño de las imagenes svg.

--- resources/views/welcome/footer.blade.php
+++ resources/views/welcome/footer.blade.php
@@ -1,63 +1,40 @@
-<footer aria-label="Site Footer" class="bg-white dark:bg-gray-800">
-    <div class="max-w-screen-xl px-4 py-10 mx-auto sm:px-6 lg:px-8">
+<footer aria-label="Site Footer" class="bg-white dark:bg-gray-800 px-6 pt-11 pb-6 lg:px-20">
         <div class="text-center">
-            <h2 class="font-medium mt-4 text-gray-500 dark:text-gray-300 text-2xl">
-                &iquest;Consultas&quest; con gusto lo atendemos
+            <h2 class="font-bold mb-3 text-gray-500 dark:text-gray-300 text-2xl">
+                Escr&iacute;banos y haga sus consultas
             </h2>
 
-            <p class="max-w-sm mx-auto mb-4 text-gray-500 dark:text-gray-300">
-                Escr&iacute;banos a traves de WhatsApp
+            <p class="text-gray-500 dark:text-gray-300 font-semibold mb-8 mx-auto max-w-sm">
+                M&aacute;ndenos un mensaje por WhatsApp o nuestras redes sociales
             </p>
             
             <a 
                 aria-label="Chat on WhatsApp"
-                class="mx-auto text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 text-sm font-medium transition py-2 px-4"
+                class="text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 text-sm font-semibold transition"
                 href="https://wa.me/50370171649"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <svg class="w-6 h-6 fill-current stroke-current align-top inline-block" aria-label="WhatsApp" viewBox="0 0 302 308"><path fill="none" stroke-width="26" d="m20 290 14-65a138 138 0 1 1 50 47z"/><path d="M205 170c-3-2-6-3-9 1l-12 16c-3 2-5 3-9 1-15-8-36-17-54-47-1-4 1-6 3-8l9-14c2-2 1-4 0-6l-12-29c-3-8-6-7-9-7h-8c-2 0-6 1-10 5-22 22-13 53 3 73 3 4 23 40 66 59 32 14 39 12 48 10 11-1 22-10 27-19 1-3 6-16 2-18"/></svg>
-                <span >Chat en Whatsapp</span>
+                <svg class="w-6 h-6 mr-1 fill-current stroke-current align-top inline-block" aria-label="WhatsApp" viewBox="0 0 302 308"><path fill="none" stroke-width="26" d="m20 290 14-65a138 138 0 1 1 50 47z"/><path d="M205 170c-3-2-6-3-9 1l-12 16c-3 2-5 3-9 1-15-8-36-17-54-47-1-4 1-6 3-8l9-14c2-2 1-4 0-6l-12-29c-3-8-6-7-9-7h-8c-2 0-6 1-10 5-22 22-13 53 3 73 3 4 23 40 66 59 32 14 39 12 48 10 11-1 22-10 27-19 1-3 6-16 2-18"/></svg>
+                <span>Ir al chat en WhatsApp</span>
             </a>
-        </div>
 
-        <div
-            class="pt-8 mt-8 border-t border-gray-100 dark:border-gray-700 flex gap-5 flex-col md:flex-row items-center justify-between"
-        >
-            <nav class="grow basis-0">
-                <ul class="flex flex-wrap gap-4">
-                    <li>
-                        <x-link :href="route('welcome.index')">
-                            {{ __('Bienvenida') }}
-                        </x-link>
-                    </li>
-                    @guest
-                    <li>
-                        <x-link :href="route('login')">
-                            {{ __('messages.login') }}
-                        </x-link>
-                    </li>
-                    @endguest
-                </ul>
-            </nav>
-
-            <ul class="flex justify-center gap-6 grow basis-0">
+            <ul class="flex justify-center gap-9 grow basis-0 mt-8">
                 <li>
                     <a
                         href="https://www.facebook.com/profile.php?id=100083341297220"
                         rel="noreferrer"
                         target="_blank"
                         rel="noopener noreferrer"
-                        class="text-gray-500 dark:text-gray-400 transition hover:text-gray-700 dark:hover:text-gray-300"
+                        class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 transition"
                     >
                         <span class="sr-only">Facebook</span>
-
                         <svg
                             class="w-6 h-6 fill-current"
-                            viewBox="0 0 20.01 16.4"
                             aria-hidden="true"
+                            viewBox="0 0 14.006464 13.915003"
                         >
-                        <path fill-rule="evenodd" d="M20.01 8.26a10 10 0 1 0-11.56 9.88v-7H5.91V8.27h2.54v-2.2c0-2.5 1.49-3.9 3.78-3.9 1.09 0 2.23.2 2.23.2v2.46h-1.25c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.45 2.89h-2.33v6.99a10 10 0 0 0 8.43-9.88z" clip-rule="evenodd"/>
+                            <path d="M14.0065 7a7 7 0 0 0-7-7 7 7 0 0 0-1.094 13.915V9.023h-1.776V7h1.777V5.458c0-1.754 1.045-2.724 2.644-2.724.766 0 1.567.137 1.567.137v1.723h-.883c-.87 0-1.14.54-1.14 1.093V7h1.941l-.31 2.023h-1.632v4.892A7.001 7.001 0 0 0 14.0065 7z"/>
                         </svg>
                     </a>
                 </li>
@@ -68,10 +45,9 @@
                         rel="noreferrer"
                         target="_blank"
                         rel="noopener noreferrer"
-                        class="text-gray-500 dark:text-gray-400 transition hover:text-gray-700 dark:hover:text-gray-300"
+                        class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 transition"
                     >
                         <span class="sr-only">Instagram</span>
-
                         <svg
                             class="w-6 h-6 fill-current"
                             viewBox="0 0 24 24"
@@ -88,7 +64,7 @@
                         rel="noreferrer"
                         target="_blank"
                         rel="noopener noreferrer"
-                        class="text-gray-500 dark:text-gray-400 transition hover:text-gray-700 dark:hover:text-gray-300"
+                        class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 transition"
                     >
                         <span class="sr-only">Tiktok</span>
                         <svg
@@ -96,18 +72,37 @@
                             viewBox="0 0 17.46 20.02"
                             aria-hidden="true"
                         >
-                        <path fill-rule="evenodd" d="M16.46 4.69A4.8 4.8 0 0 1 12.7.44V0H9.24v13.67a2.9 2.9 0 0 1-5.2 1.75 2.9 2.9 0 0 1 3.2-4.52V7.4a6.33 6.33 0 0 0-5.4 10.7 6.33 6.33 0 0 0 10.85-4.43V6.7a8.18 8.18 0 0 0 4.77 1.52V4.8a4.83 4.83 0 0 1-1-.1z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M16.46 4.69A4.8 4.8 0 0 1 12.7.44V0H9.24v13.67a2.9 2.9 0 0 1-5.2 1.75 2.9 2.9 0 0 1 3.2-4.52V7.4a6.33 6.33 0 0 0-5.4 10.7 6.33 6.33 0 0 0 10.85-4.43V6.7a8.18 8.18 0 0 0 4.77 1.52V4.8a4.83 4.83 0 0 1-1-.1z" clip-rule="evenodd"/>
                         </svg>
                     </a>
                 </li>
             </ul>
+        </div>
 
-            <div class="text-gray-500 dark:text-gray-400 text-sm font-medium text-right grow basis-0">
-                ©
-                <script>document.write(new Date().getFullYear());</script>
-                Copyright:
-                <span class="text-gray-700 dark:text-gray-300 inline">CSGarage</span>
+        <div
+            class="border-t border-gray-300 dark:border-gray-700 flex flex-col gap-6 items-center justify-between pt-6 mt-9 md:flex-row"
+        >
+            <nav>
+                <ul class="flex flex-wrap gap-6">
+                    <li>
+                        <x-link :href="route('welcome.index')">
+                            {{ __('Bienvenida') }}
+                        </x-link>
+                    </li>
+                    @guest
+                    <li>
+                        <x-link :href="route('login')">
+                            {{ __('messages.login') }}
+                        </x-link>
+                    </li>
+                    @endguest
+                </ul>
+            </nav>
+
+            <div class="text-gray-500 dark:text-gray-400 text-sm font-medium text-right">
+                &copy; <script>document.write(new Date().getFullYear())</script>,
+                <span class="text-gray-700 dark:text-gray-300 inline">CSGarage</span>.
+                Todos los derechos reservados.
             </div>
         </div>
-    </div>
 </footer>
