da591cddca011efa58ccee3867fb73a9f7991fc6
Miguel <m.alejnd.lpz@gmail.com>
2023-01-18 21:37:59 -0600

    Footer: Cambia texto del botón de WhatsApp
    Cambia el texto 'Ir al chat en WhatsApp' por 'Abrir chat en WhatsApp'.

--- resources/views/welcome/footer.blade.php
+++ resources/views/welcome/footer.blade.php
@@ -16,7 +16,7 @@
                 rel="noopener noreferrer"
             >
                 <svg class="w-6 h-6 mr-1 fill-current stroke-current align-top inline-block" aria-label="WhatsApp" viewBox="0 0 302 308"><path fill="none" stroke-width="26" d="m20 290 14-65a138 138 0 1 1 50 47z"/><path d="M205 170c-3-2-6-3-9 1l-12 16c-3 2-5 3-9 1-15-8-36-17-54-47-1-4 1-6 3-8l9-14c2-2 1-4 0-6l-12-29c-3-8-6-7-9-7h-8c-2 0-6 1-10 5-22 22-13 53 3 73 3 4 23 40 66 59 32 14 39 12 48 10 11-1 22-10 27-19 1-3 6-16 2-18"/></svg>
-                <span>Ir al chat en WhatsApp</span>
+                <span>Abrir chat en WhatsApp</span>
                 <svg width="8" height="15" class="inline-block fill-current"><path d="M.52 1.91 5.1 6.5.52 11.09l1.4 1.41 6-6-6-6z"/></svg>
             </a>
 
