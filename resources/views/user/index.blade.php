b7a33002da505766371ad53941bee0ceccb96e18
Miguel <m.alejnd.lpz@gmail.com>
2023-02-14 12:09:33 -0600

    Refactoriza el codigo fuente de las vistas
    Simplificaciones: elimina elementos html innecesarios,
     así como clases innecesarias,
     dar orden y buen formato al código fuente.

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,30 +7,30 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 lg:px-8 mb-5">
-            <div>
-                <div class="text-gray-700 dark:text-gray-300">
-                    <x-link-button href="{{ route('usuarios.create') }}">
-                        Nuevo usuario
-                    </x-link-button>
-                </div>
-            </div>
+            <x-link-button href="{{ route('usuarios.create') }}">
+                Nuevo usuario
+            </x-link-button>
         </div>
-        <div class="max-w-7xl mx-auto px-4 lg:px-8">
-            <div class="relative overflow-x-auto rounded-lg border border-gray-300 dark:border-gray-700">
+
+        <div class="max-w-7xl mx-auto px-4 lg:px-8 space-y-5">
+            <div class="overflow-x-auto rounded-lg border border-gray-300 dark:border-gray-700">
                 <table class="table-fixed min-w-full text-sm text-left text-gray-900 dark:text-gray-100">
                     <thead>
                         <tr>
                             <th scope="col" class="px-6 py-3 bg-white dark:bg-gray-800">
                                 Nombre
                             </th>
+                            
                             <th scope="col" class="px-6 py-3">
                                 Correo electrónico
                             </th>
+
                             <th scope="col" class="px-6 py-3 bg-white dark:bg-gray-800">
                                 Acciones
                             </th>
                         </tr>
                     </thead>
+                    
                     <tbody>
                         @foreach($users as $user)
                             <tr class="border-t border-gray-300 dark:border-gray-700">
@@ -47,6 +47,7 @@
                                     >
                                         Detalles
                                     </a>
+
                                     <a
                                         href="{{ route('usuarios.edit', $user->id) }}"
                                         class="text-orange-600 dark:text-orange-400 hover:underline"
@@ -59,9 +60,8 @@
                     </tbody>
                 </table>
             </div>
-            <div class="mt-5">
-                {{ $users->links() }}
-            </div>
+
+            {{ $users->links() }}
         </div>
     </div>
 </x-app-layout>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -2,7 +2,15 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             <a href="{{ route('usuarios.index') }}">{{ __('messages.users') }}</a>
-            <svg class="inline overflow-visible mx-1.5 fill-none stroke-current stroke-2 w-auto h-1.5" viewBox="0 0 3 6" stroke-linejoin="round" stroke-linecap="round"><path d="M0 0L3 3L0 6"></path></svg>
+
+            <svg 
+                class="inline overflow-visible mx-1.5 fill-none stroke-current stroke-2 w-auto h-1.5"
+                viewBox="0 0 3 6"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+            >
+                <path d="M0 0L3 3L0 6" />
+            </svg>
             Ver detalles
         </h2>
     </x-slot>
@@ -10,37 +18,35 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="text-gray-900 dark:text-gray-100 font-medium">
+                <div class="text-gray-900 dark:text-gray-100 font-medium text-sm">
                     <section>
                         <header>
                             <h2 class="text-lg">
-                                {{ __('Detalles del usuario') }}
+                                Detalles del usuario
                             </h2>
 
-                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                {{ __('La información de esta cuenta se presenta a continuación.') }}
+                            <p class="mt-1 text-gray-600 dark:text-gray-400">
+                                La información de esta cuenta se presenta a continuación.
                             </p>
                         </header>
 
-                        <div class="border-t border-gray-300 dark:border-gray-700 mt-6">
+                        <div class="my-6">
                             <dl>
-                                <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4">
-                                    <dt class="text-sm text-gray-600 dark:text-gray-400">Nombre</dt>
-                                    <dd class="mt-1 text-sm sm:col-span-2 sm:mt-0">{{ $user->name }}</dd>
+                                <div class="p-4 sm:grid sm:grid-cols-3 sm:gap-4 border-y border-gray-300 dark:border-gray-700">
+                                    <dt class="text-gray-600 dark:text-gray-400">Nombre</dt>
+                                    <dd class="sm:col-span-2 sm:mt-0">{{ $user->name }}</dd>
                                 </div>
 
-                                <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 border-t border-gray-300 dark:border-gray-700">
-                                    <dt class="text-sm text-gray-600 dark:text-gray-400">Correo electrónico</dt>
-                                    <dd class="mt-1 text-sm sm:col-span-2 sm:mt-0">{{ $user->email }}</dd>
+                                <div class="p-4 sm:grid sm:grid-cols-3 sm:gap-4 border-b border-gray-300 dark:border-gray-700">
+                                    <dt class="text-gray-600 dark:text-gray-400">Correo electrónico</dt>
+                                    <dd class="sm:col-span-2 sm:mt-0">{{ $user->email }}</dd>
                                 </div>
                             </dl>
-
-                            <div class="border-t border-gray-300 dark:border-gray-700 pt-6">
-                                <x-link-button href="{{ route('usuarios.edit', $user->id) }}">
-                                    Editar información
-                                </x-link-button>
-                            </div>
                         </div>
+
+                        <x-link-button href="{{ route('usuarios.edit', $user->id) }}">
+                                    Editar información
+                         </x-link-button>
                     </section>
                 </div>
             </div>
